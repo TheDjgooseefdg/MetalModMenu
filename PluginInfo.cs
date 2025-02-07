df10be01bf657b66c816af576dc346eb9183a179
Djgoose <djgoosestudios@gmail.com>
2025-02-07 00:04:17 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.Metal.gorillatag.Metaltemplate";
+        public const string Name = "🔩 Mod Menu🔩";
+        public const string Description = "Created by @vipenz_23365 with love <3";
+        public const string Version = "1.0.0";
+    }
+}
